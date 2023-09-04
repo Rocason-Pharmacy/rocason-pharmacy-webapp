04d9c612b6c3409be3549d7007b4474d83f2d0b3
Gregory Otokhina <gregoryotokhina@gmail.com>
2023-09-04 12:06:04 +0100

    Adding links to button

--- src/Components/Home/ProductCategory.tsx
+++ src/Components/Home/ProductCategory.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Pills from '../../assets/home/pills.png'
 import Wellman from '../../assets/home/wellman.png'
 const ProductCategory = () => {
@@ -11,7 +12,9 @@ const ProductCategory = () => {
             <div className='medic-section'>
               <h2>Prescription Medicines</h2>
               <p>Your Choice For Quality and <br/>Affordable Drugs.</p>
-              <button className='buynow-btn'>Buy Now</button>
+              <Link to='/'>
+                 <button className='buynow-btn'>Buy Now</button>
+              </Link>
               <div className='img-container'>
               <img src={Pills} alt='' />
               </div>
@@ -19,18 +22,24 @@ const ProductCategory = () => {
             <div className='medic-section-b'>
               <div className='medic-bg'>
               <h2>OTC</h2>
-              <button className='buynow-btn-b'>Buy Now</button>
+              <Link to=''>
+                  <button className='buynow-btn-b'>Buy Now</button>
+              </Link>
               </div>
             </div>
             <div className='medic-section-c'>
             <div className='medic-bg-c'>
               <h2>Cold/Flu</h2>
-              <button className='buynow-btn-c'>Buy Now</button>
+              <Link to=''>
+                  <button className='buynow-btn-c'>Buy Now</button>
+              </Link>
               </div>
             </div>
             <div className='medic-section-d'>
             <h2>Supplements</h2>
-              <button className='buynow-btn-d'>Buy Now</button>
+               <Link to=''>
+                    <button className='buynow-btn-d'>Buy Now</button>
+              </Link>
               <div className='img-container-d'>
               <img src={Wellman} alt='' />
               </div>
