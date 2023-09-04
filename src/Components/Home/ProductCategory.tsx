e97acb2d079e5d484701a54fb41744c44db88a39
Gregory Otokhina <gregoryotokhina@gmail.com>
2023-09-04 07:44:14 +0100

    Adding the product category

---
+++ src/Components/Home/ProductCategory.tsx
@@ -0,0 +1,25 @@
+const ProductCategory = () => {
+  return (
+    <div>
+        <div className='product-header'>
+            <h2>Product Category</h2>
+            <hr/>
+        </div>
+        <div className='product-container'>
+            <div className='medic-section'>
+              <h2>Prescription Medicines</h2>
+              <p>Your Choice For Quality and <br/>Affordable Drugs.</p>
+              <button className='buynow-btn'>Buy Now</button>
+            </div>
+            <div className='medic-section-b'>
+            </div>
+            <div className='medic-section-c'>
+            </div>
+            <div className='medic-section-d'>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCategory
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
 import Order from '../Components/Home/Order';
 import ProRec from '../Components/Home/ProRec';
+import ProductCategory from '../Components/Home/ProductCategory';
+import Footer from '../Components/Layout/Footer';
+import Header from '../Components/Layout/Header';
 
 const HomePage = () => {
   return (
     <div>
+          <Header/>
+          <ProductCategory/>
           <Order />
           <ProRec/>
+          <Footer/>
     </div>
   );
 }
