ffc882ca90c98a4f60516d088f85157323b80ffe
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-09-06 14:14:40 +0100

    about-page
    responsive welcome component

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import HeroBanner from '../Components/about/HeroBanner';
+import Welcome from '../Components/about/Welcome';
 
 const AboutUs: FC = () => {
   return (
     <div>
       <HeroBanner />
+      <Welcome />
     </div>
   );
 };
