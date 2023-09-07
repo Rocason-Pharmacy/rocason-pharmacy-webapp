25032e4a57033492e813375329b225070bce8e36
Fortune Ellah <ellfortune3@gmail.com>
2023-09-07 19:52:19 +0100

    adding media queries to services page

--- src/Components/Services/ServiceCards.tsx
+++ src/Components/Services/ServiceCards.tsx
@@ -5,15 +5,21 @@ import Hospital from '../../assets/services/hospital-bed 1.png'
 import Lotion from '../../assets/services/lotion 1.png'
 import Prescription from '../../assets/services/medical-prescription 1.png'
 import Therapy from '../../assets/services/pharmacist 1.png'
+import Vector from '../../assets/services/Vector.svg'
+import Svg from '../../assets/services/Ellipse 1.svg'
+import Svg2 from '../../assets/services/Ellipse 2.svg'
 import '../../styles/services/serviceCard.css'
 const ServiceCards = () => {
    
   return (
-    <div>
+    <div className='service-container'>
+        <div className='service-card-header'>
+            <div>
+            <h2>Our services include:</h2>
+            <hr/>
+            </div>
+        </div>
       <div className='service-cards-container'>
-        <h2>Our services include:</h2>
-        <hr/>
-     
         <CustomServiceCards img={Phone} title={'Online Pharmacy'} desc={'Order and purchase your drugs online, Rocason is Always Available to Serve You, Regardless of Distance'}/>
         <CustomServiceCards img={Delivery} title={'Prescription Refill'} desc={'Our Dedicated Professionals Are Always Available to Assess Your Medications'}/>
         <CustomServiceCards img={Hospital} title={'Skincare products'} desc={'We also provide Skincare products, cosmetics and toiletries.'}/>
@@ -21,6 +27,11 @@ const ServiceCards = () => {
         <CustomServiceCards img={Prescription} title={'Quick Delivery Nationwide'} desc={'Swift delivery to your door. Convenience and care, anytime, anywhere'}/>
         <CustomServiceCards img={Therapy} title={'Counseling'} desc={'We provide professional information and counselling and drugs and medication, ensuring proper use of medication.'}/>
        
+      </div>
+      <div className='svg-container'>
+        <img src={Vector} alt="" />
+        <img src={Svg} alt="" />
+        <img src={Svg2} alt="" />
       </div>
     </div>
   )
