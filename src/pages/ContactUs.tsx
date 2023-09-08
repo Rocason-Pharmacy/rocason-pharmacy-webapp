fabf426277cfb80ac92d58d1c4d6a99db34a3316
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-09-08 04:53:30 +0100

    contact-page/contact map
    mobile and desktop view

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import HeroBanner from './../Components/contact/HeroBanner';
 import WorkingHrs from '../Components/contact/WorkingHrs';
+import ContactMap from '../Components/contact/Map';
 
 const ContactUs: FC = () => {
   return (
     <div>
       <HeroBanner />
       <WorkingHrs />
+      <ContactMap />
     </div>
   );
 };
