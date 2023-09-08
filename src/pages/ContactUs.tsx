7f0efb540c9aa233857b49cb5c9827843874926a
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-09-08 01:42:40 +0100

    contact us/ working hrs component
    mobile and desktop view

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
-import HeroBanner from '../Components/contact/HeroBanner';
+import HeroBanner from './../Components/contact/HeroBanner';
+import WorkingHrs from '../Components/contact/WorkingHrs';
 
 const ContactUs: FC = () => {
   return (
     <div>
       <HeroBanner />
+      <WorkingHrs />
     </div>
   );
 };
