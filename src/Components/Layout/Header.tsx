640c4cd556eba851601087deb87980c5a0dd6ed9
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-09-12 09:32:46 +0100

    navbar desktop screen

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -1,67 +1,41 @@
-import '../Layout/header.css'
-import Logo from '../../assets/logo.png'
-import CloseBtn from '../../assets/home/close.png'
-import {BiPhone} from 'react-icons/bi'
-import { IoMdMenu } from "react-icons/io";
+import '../Layout/header.css';
+import Logo from '../../assets/logo.png';
+import CloseBtn from '../../assets/home/close.png';
+import { FaPhone } from 'react-icons/fa';
+import { IoMdMenu } from 'react-icons/io';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-  const [isNavOpen, getIsNavOpen] = useState(false);
+  // const [isNavOpen, getIsNavOpen] = useState(false);
 
   return (
-    <div className='header'>
-        <NavLink to='/'>
-          <img src={Logo} alt="" />
-        </NavLink>
-        <span className='header-contact'>
-         <BiPhone/>
+    <nav className="navbar">
+      <NavLink to="/">
+        <img src={Logo} alt="rocason-logo" />
+      </NavLink>
+
+      <ul className="nav-list">
+        <li>
+          <NavLink to="/about">About Us</NavLink>
+        </li>
+        <li>
+          <NavLink to="/service">Services</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Products</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact Us</NavLink>
+        </li>
+
+        <span className="header-contact">
+          <FaPhone />
           <a href="tel:01623 590463">+234 803 856 9395</a>
         </span>
-      <div
-          className="mobile-menu"
-          onClick={() => getIsNavOpen((prev) => !prev)}
-        >
-          <IoMdMenu className="hamburger" />
-        </div>
-        <div className={isNavOpen ? "show-menu" : "hide-menu"}>
-          <div className="close-btn" onClick={() => getIsNavOpen(false)}>
-            <img src={CloseBtn} alt="" className='close' />
-          </div>
-          <ul className="mobile-nav">
-            <NavLink to="/">
-              <li>Home</li>
-            </NavLink>
-            <NavLink to="/about">
-              <li>About Us</li>
-            </NavLink>
-            <NavLink to="/service">
-              <li>Services</li>
-            </NavLink>
-            <NavLink to="/product">
-            <li>Products</li>
-          </NavLink>
-            <NavLink to="/contact">
-              <li>Contact Us</li>
-            </NavLink>
-          </ul>
-        </div>
-        <ul className="desktop-nav">
-          <NavLink to="/about">
-            <li>About Us</li>
-          </NavLink>
-          <NavLink to="/services">
-            <li>Services</li>
-          </NavLink>
-          <NavLink to="/product">
-            <li>Products</li>
-          </NavLink>
-          <NavLink to="/contact">
-            <li>Contact Us</li>
-          </NavLink>
-        </ul>
-    </div>
-  )
-}
+      </ul>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
