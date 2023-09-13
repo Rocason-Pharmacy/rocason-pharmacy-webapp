d5c814e37d541439a22764817d8d186f003b1f76
Abiodun Segun <abiodun_segun10@yahoo.com>
2023-09-13 12:43:29 +0100

    fix button on order component
    linkable to contact page

--- src/Components/Home/Order.tsx
+++ src/Components/Home/Order.tsx
@@ -1,26 +1,22 @@
-import "../../styles/home/order.css";
-import orderDrugs from '../../assets/home/orderDrugs.png'
+import '../../styles/home/order.css';
+import orderDrugs from '../../assets/home/orderDrugs.png';
+import { Link } from 'react-router-dom';
 
 const Order = () => {
-    return (
-      <div className="ordercontainer">
-        <div className="order">
-          <div className="order-text">
-            <h1>Order Your Drugs</h1>
-            <p>Send us a message for your medications</p>
-          </div>
-            <button className="order-button"> 
-              <a href="https://wa.me/+2348038569395"
-                 target="_blank"
-                 rel="noopener noreferrer">
-                  Contact Us
-              </a>  
-            </button>
-          
+  return (
+    <div className="ordercontainer">
+      <div className="order">
+        <div className="order-text">
+          <h1>Order Your Drugs</h1>
+          <p>Send us a message for your medications</p>
         </div>
-            <img src={orderDrugs} alt="" />
+        <Link to="/contact">
+          <button className="order-button">Contact Us</button>
+        </Link>
       </div>
-    );
+      <img src={orderDrugs} alt="" />
+    </div>
+  );
 };
 
 export default Order;
